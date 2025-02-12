4f7f5e17c75a42553178e0b24052470ebeff9538
Pravallikag123 <166217778+Pravallikag123@users.noreply.github.com>
2025-02-12 18:02:04 +0530

    server.js

--- server.js
+++ server.js
@@ -13,12 +13,10 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
-// MongoDB Connection
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log("Error: ", err));
 
-// Routes will be defined later
 app.get("/", (req, res) => {
   res.send("Inventory Management System Backend");
 });
