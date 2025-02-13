fffa882f42206e7541c51a220b778ff404b655c4
Pravallikag123 <166217778+Pravallikag123@users.noreply.github.com>
2025-02-13 12:14:44 +0530

    server.js

--- server.js
+++ server.js
@@ -4,12 +4,11 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
 
-dotenv.config();  // To use the .env file for configuration
+dotenv.config();  
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-// Middleware
 app.use(cors());
 app.use(bodyParser.json());
 
