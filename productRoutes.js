43643b772ed3ecbe385c1f561f436dcd98796150
Pravallikag123 <166217778+Pravallikag123@users.noreply.github.com>
2025-02-13 12:14:12 +0530

    productRoutes.js

--- productRoutes.js
+++ productRoutes.js
@@ -2,8 +2,6 @@ const express = require("express");
 const Product = require("../models/Product");
 
 const router = express.Router();
-
-// Create a new product
 router.post("/add", async (req, res) => {
   const { name, quantity, price } = req.body;
   try {
@@ -15,7 +13,7 @@ router.post("/add", async (req, res) => {
   }
 });
 
-// Get all products
+
 router.get("/", async (req, res) => {
   try {
     const products = await Product.find();
@@ -35,8 +33,6 @@ router.put("/:id", async (req, res) => {
     res.status(400).json({ error: err.message });
   }
 });
-
-// Delete product
 router.delete("/:id", async (req, res) => {
   try {
     const deletedProduct = await Product.findByIdAndDelete(req.params.id);
